a95afa80f0509130e1e4ca9fd14d89550a77daf0
Andrew jeon <andrew.jeon@box.co.kr>
2026-05-06 22:34:31 +0900

    Update routine to v1.0.4

--- Casks/routine.rb
+++ Casks/routine.rb
@@ -1,6 +1,6 @@
 cask "routine" do
-  version "1.0.3"
-  sha256 "cc3719f566888d1432efb013e927d8ba95d6258a72f9bd191546b1919721339d"
+  version "1.0.4"
+  sha256 "1d42ec2c861ea819b82b5c9d25f6eaff0eba5a41aa827bca7f7a2538d2b301a4"
 
   url "https://github.com/box-kr/routine/releases/download/v#{version}/Routine-#{version}.zip"
   name "Routine"
